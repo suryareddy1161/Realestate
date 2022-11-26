0e1421b1686fbbc8abe91d67f30d607c30db69f3
suryareddy <suryareddy1161@gmail.com>
2022-11-26 15:23:27 +0530

    final

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -1,22 +1,40 @@
-import React from 'react';
-import "./Navbar1.css";
+import React, { useState } from 'react';
+import "./Navbar.css";
 import { Link, useNavigate } from "react-router-dom";
+import {FaBars, FaTimes} from "react-icons/fa"
 function Navbar() {
-  let menu =document.querySelector(".menu")
-  let toggle = document.querySelector(".toggle")
-  const toggleClick=()=>{
-      menu.classList.toggle("active")
+ /*  const menu =document.querySelector(".menu")
+  const toggle1 = ()=>{
+    document.querySelector(".toggle1")
   }
-  const navigate = useNavigate();
+  const toggleClick=()=>{
+      menu.className.toggle1("active")
+  } */
+  const [click,setClick]= useState(false)
+ /*  
+  const changecolor = () =>{
+      if(window.scroll >=1){
+          setColor(true)
+      }
+      else{
+          setColor(false)
+      }
+  }
+  window.addEventListener("scroll",changecolor) */
+  const handleClick=()=>{
+      setClick(!click)
+  }
+ 
 
   return (
-    <div className='navbar1'>
-    <div className='logo1'>Estate</div>
-    <div className='toggle' onClick={toggleClick}>
-    <i className='fas fa-bars ' ></i>
-    </div>
-    <div className='menu'>
-    <ul>
+    <div className='navbar'>
+    <div className='logo'>Estate</div>
+    <div className='hamburger' onClick={handleClick}>
+                {click ? (<FaTimes  style={{color:'black'}} />)
+                :(<FaBars  style={{color:"black"}} />)}
+            </div>
+    <div className='navmenu'>
+    <ul className={click ? "navmenu active" : 'navmenu '}>
         <li><a href='/'>Home</a></li>
         <li><a>Rent</a></li>
         <li><a>Sell</a></li>
@@ -25,7 +43,7 @@ function Navbar() {
         <li><a  href='#'>login</a></li> 
         <li><a  href='#'>Sign-up</a></li>
     </ul>
-    
+   
     </div> 
    
 </div>
